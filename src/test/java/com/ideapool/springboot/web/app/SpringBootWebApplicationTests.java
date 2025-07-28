654887fd3dc378d06257bdce98a8f1172760482c
YaelGuillermo <l20211785@tectijuana.edu.mx>
2025-07-27 20:40:16 -0700

    first commit

---
+++ src/test/java/com/ideapool/springboot/web/app/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideapool.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
